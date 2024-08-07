3f4a2c0f866149009d78ed4672d2c949ccf034ef
PriyavKaneria <priyavkaneria@gmail.com>
2024-08-07 15:25:34 +0530

    Generated basic configs

--- configs/arrow5x5.py
+++ configs/arrow5x5.py
@@ -1,40 +1,40 @@
 node_config = {
-    (0, 0.5): "tanh",      # Input node
+    (0, 0.5): "relu",      # Input node
 
     # Hidden layer 1 (5 nodes)
-    (1, 0.1): "tanh",
-    (1.1, 0.3): "tanh",
-    (1.2, 0.5): "tanh",
-    (1.1, 0.7): "tanh",
-    (1, 0.9): "tanh",
+    (1, 0.1): "relu",
+    (1.1, 0.3): "relu",
+    (1.2, 0.5): "relu",
+    (1.1, 0.7): "relu",
+    (1, 0.9): "relu",
 
     # Hidden layer 2 (5 nodes)
-    (2, 0.1): "tanh",
-    (2.1, 0.3): "tanh",
-    (2.2, 0.5): "tanh",
-    (2.1, 0.7): "tanh",
-    (2, 0.9): "tanh",
+    (2, 0.1): "relu",
+    (2.1, 0.3): "relu",
+    (2.2, 0.5): "relu",
+    (2.1, 0.7): "relu",
+    (2, 0.9): "relu",
 
     # Hidden layer 3 (5 nodes)
-    (3, 0.1): "tanh",
-    (3.1, 0.3): "tanh",
-    (3.2, 0.5): "tanh",
-    (3.1, 0.7): "tanh",
-    (3, 0.9): "tanh",
+    (3, 0.1): "relu",
+    (3.1, 0.3): "relu",
+    (3.2, 0.5): "relu",
+    (3.1, 0.7): "relu",
+    (3, 0.9): "relu",
 
     # Hidden layer 4 (5 nodes)
-    (4, 0.1): "tanh",
-    (4.1, 0.3): "tanh",
-    (4.2, 0.5): "tanh",
-    (4.1, 0.7): "tanh",
-    (4, 0.9): "tanh",
+    (4, 0.1): "relu",
+    (4.1, 0.3): "relu",
+    (4.2, 0.5): "relu",
+    (4.1, 0.7): "relu",
+    (4, 0.9): "relu",
 
     # Hidden layer 5 (5 nodes)
-    (5, 0.1): "tanh",
-    (5.1, 0.3): "tanh",
-    (5.2, 0.5): "tanh",
-    (5.1, 0.7): "tanh",
-    (5, 0.9): "tanh",
+    (5, 0.1): "relu",
+    (5.1, 0.3): "relu",
+    (5.2, 0.5): "relu",
+    (5.1, 0.7): "relu",
+    (5, 0.9): "relu",
 
     (6, 0.5): "linear"       # Output node
 }
--- configs/basic2x4.py
+++ configs/basic2x4.py
@@ -2,14 +2,22 @@ node_config = {
     (0, 0.5): "relu",      # Input node
 
     # Hidden layer 1 (2 nodes)
-    (1, 0.1): "relu",
-    (1, 0.9): "relu",
+    (1, 0.1): "tanh",
+    (1, 0.9): "tanh",
 
     # Hidden layer 2 (2 nodes)
     (2, 0.1): "tanh",
     (2, 0.9): "tanh",
     
-    (3, 0.5): "linear"       # Output node
+    # Hidden layer 3 (2 nodes)
+    (3, 0.1): "tanh",
+    (3, 0.9): "tanh",
+    
+    # Hidden layer 4 (2 nodes)
+    (4, 0.1): "tanh",
+    (4, 0.9): "tanh",
+    
+    (5, 0.5): "relu"       # Output node
 }
 
 node_positions = list(node_config.keys())
